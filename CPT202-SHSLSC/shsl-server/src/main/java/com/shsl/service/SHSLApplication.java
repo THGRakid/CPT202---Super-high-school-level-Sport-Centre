b41fdd3f6febba6034ed0ecf62e6a7917543fc34
cblem0n <1491848984@qq.com>
2024-04-24 21:28:23 +0800

    舒服

--- CPT202-SHSLSC/shsl-server/src/main/java/com/shsl/service/SHSLApplication.java
+++ CPT202-SHSLSC/shsl-server/src/main/java/com/shsl/service/SHSLApplication.java
@@ -1,18 +1,18 @@
-
-package com.shsl;
+package com.shsl.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement //开启注解方式的事务管理
+@EnableTransactionManagement // 开启注解方式的事务管理
 @Slf4j
+@MapperScan("com.shsl.mapper")
 public class SHSLApplication {
     public static void main(String[] args) {
         SpringApplication.run(SHSLApplication.class, args);
-        log.info("server started");
+        log.info("Server started");
     }
 }
-
